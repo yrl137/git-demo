f20272917153d742283f82c67a118341ecd760db
yrl <yurulo@163.com>
2022-02-07 19:39:30 +0800

    master 3 commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Git-idea2");
-        System.out.println("Git-idea2 test");
+        System.out.println("Git-idea2 master");
 
     }
 }
